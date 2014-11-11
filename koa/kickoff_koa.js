7740eb808ff63be3486fdde2fd9029f4e4c22af4
Ernie Casilla <ecasilla@icloud.com>
2014-11-10 22:19:53 -0300

    finsihed kickoff

--- koa/kickoff_koa.js
+++ koa/kickoff_koa.js
@@ -2,8 +2,8 @@ var koa = require('koa'),
   app = koa(),
   port = process.argv[2];
 
-app.use(function(){
-  this.body = 'Hello';
+app.use(function *(){
+  this.body = 'hello koa';
 })
 
 app.listen(port);
